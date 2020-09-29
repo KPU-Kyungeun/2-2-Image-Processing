cc1e6940841cb976b1a1ac8c2bb99bb80f6e22e5
박경은 <ruddms936@naver.com>
2020-09-29 18:48:16 +0900

    feat: test2.cpp 추가

---
+++ ch03/test2.cpp
@@ -0,0 +1,19 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat img = imread("../resources/ch03/opencv.png");
+	if (img.empty())
+	{
+		cout << "영상을 읽을 수 없음" << endl;
+		return -1;
+	}
+	imshow("img", img);
+	cout << img << endl;
+	waitKey(0);
+	return 0;
+}
