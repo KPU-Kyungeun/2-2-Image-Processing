8c5614dafa3bcdd824d24bf70354d20374b53dbc
박경은 <ruddms936@naver.com>
2020-10-20 20:10:09 +0900

    feat: resources 경로 수정

--- ch06/gaussian.cpp
+++ ch06/gaussian.cpp
@@ -1,16 +1,17 @@
-//#include <opencv2/opencv.hpp>
-//using namespace cv;
-//
-//int main()
-//{
-//	Mat src = imread("lenna.jpg", 1);
-//	Mat dst;
-//	imshow("src", src);
-//
-//	for (int i = 1; i<61; i = i + 2)
-//	{
-//		GaussianBlur(src, dst, Size(i, i), 0, 0);
-//		imshow("Gaussian filter", dst);
-//		waitKey(1000);
-//	}
-//}
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	Mat src = imread("../resources/ch01/lenna.jpg", 1);
+	Mat dst;
+	imshow("src", src);
+
+	for (int i = 1; i<61; i = i + 2)
+	{
+		GaussianBlur(src, dst, Size(i, i), 0, 0);
+		imshow("Gaussian filter", dst);
+		waitKey(1000);
+	}
+}
--- ch06/mean.cpp
+++ ch06/mean.cpp
@@ -1,17 +1,18 @@
-//#include "opencv2/opencv.hpp"
-//#include <iostream>
-//using namespace cv;
-//using namespace std;
-//
-//int main()
-//{
-//	Mat src = imread("dog.jpg");
-//	Mat dst;
-//
-//	blur(src, dst, Size(11, 11));
-//	imshow("source", src);
-//	imshow("result", dst);
-//
-//	waitKey(0);
-//	return 0;
-//}
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src = imread("../resources/ch01/dog.jpg");
+	Mat dst;
+
+	blur(src, dst, Size(11, 11));
+	imshow("source", src);
+	imshow("result", dst);
+
+	waitKey(0);
+	return 0;
+}
