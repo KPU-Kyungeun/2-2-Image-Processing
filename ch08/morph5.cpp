a6f35c997966e1135c932d979441949b25be1b61
박경은 <ruddms936@naver.com>
2020-11-10 18:56:37 +0900

    feat: 8주차 수정

--- ch08/morph5.cpp
+++ ch08/morph5.cpp
@@ -1,18 +1,19 @@
-//#include "opencv2/opencv.hpp"
-//#include <iostream>
-//using namespace cv;
-//using namespace std;
-//
-//int main()
-//{
-//	Mat src, dst, open, close;
-//	src = imread("letterj.png", IMREAD_GRAYSCALE);
-//
-//	imshow("src", src);
-//
-//	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
-//	morphologyEx(src, open, MORPH_GRADIENT, element);
-//	imshow("Gradient Demo", open);
-//	waitKey(0);
-//	return 0;
-//}
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src, dst, open, close;
+	src = imread("../resources/ch03/letterj.png", IMREAD_GRAYSCALE);
+
+	imshow("src", src);
+
+	Mat element = getStructuringElement(MORPH_RECT, Size(5, 5));
+	morphologyEx(src, open, MORPH_GRADIENT, element);
+	imshow("Gradient Demo", open);
+	waitKey(0);
+	return 0;
+}
