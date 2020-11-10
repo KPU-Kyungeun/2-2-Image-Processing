1446a7488c2874cf61fe7db9e69aeda47ef6b666
박경은 <ruddms936@naver.com>
2020-11-10 18:48:25 +0900

    init: ch08

---
+++ ch08/morph1.cpp
@@ -0,0 +1,23 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	Mat src, dst, erosion_dst, dilation_dst;
+	src = imread("../resources/ch03/test1.jpg", IMREAD_GRAYSCALE);
+
+	threshold(src, dst, 127, 255, THRESH_BINARY);
+	imshow("dst", dst);
+
+	Mat element = getStructuringElement(MORPH_RECT,
+		Size(3, 3),
+		Point(-1, -1));
+
+	erode(dst, erosion_dst, element);
+	imshow("Erosion Demo", erosion_dst);
+	waitKey(0);
+	return 0;
+}
